7deb8d4148932083460685a62e1a72016278018f
Santiago Forero <saforeros@unal.edu.co>
2025-01-14 12:50:24 -0500

    update main.py and finish parte1

--- parte-1/main.py
+++ parte-1/main.py
@@ -1,13 +1,29 @@
-from Shape import rectangle, square, circle
+"""
+This code demonstrates the usage of the Shape module.
 
-rectangle = Rectangle(2, 3)
-print(f"Area del rectángulo: {rectangle.compute_area()} unidades cuadradas")
-print(f"Perímetro del rectángulo: {rectangle.compute_perimeter()} unidades")
+Usage:
+- Import the Rectangle, Square, and Circle classes from the Shape.shapes module.
+- Create instances of Rectangle, Square, and Circle with the desired dimensions.
+- Use the compute_area() method to calculate the area of each shape.
+- Use the compute_perimeter() method to calculate the perimeter of each shape.
+- Print the results.
 
-square = Square(2)
-print(f"Area del cuadrado: {square.compute_area()} unidades cuadradas")
-print(f"Perímetro del cuadrado: {square.compute_perimeter()} unidades")
+Example:
 
-circle = Circle(81)
-print(f"Area del círculo: {circle.compute_area()} unidades cuadradas")
-print(f"Perímetro del círculo: {circle.compute_perimeter()} unidades")
+
+"""
+
+from Shape.shapes import Rectangle, Square, Circle
+
+
+rect = Rectangle(2, 3)
+print(f"Area del rectángulo: {rect.compute_area()} unidades cuadradas")
+print(f"Perímetro del rectángulo: {rect.compute_perimeter()} unidades")
+
+sq = Square(2)
+print(f"Area del cuadrado: {sq.compute_area()} unidades cuadradas")
+print(f"Perímetro del cuadrado: {sq.compute_perimeter()} unidades")
+
+circ = Circle(81)
+print(f"Area del círculo: {circ.compute_area()} unidades cuadradas")
+print(f"Perímetro del círculo: {circ.compute_perimeter()} unidades")
